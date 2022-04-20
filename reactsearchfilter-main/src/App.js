63eb58faaf0559ba21994eb779213045d5cde541
Md Jamal Uddin <43787665+Jamal161@users.noreply.github.com>
2022-04-21 04:12:30 +0600

    Add files via upload

---
+++ reactsearchfilter-main/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <>
+     <Search />
+    </>
+   
+  );
+}
+
+export default App;
